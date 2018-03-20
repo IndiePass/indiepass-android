53a539bdf4322e3b8aa754db3ef029c33b605a60
swentel <swentel@realize.be>
2018-03-20 20:12:15 +0000

    Use bottomsheet for fast actions like creating notes, posts etc

--- app/src/main/java/com/indieweb/indigenous/TimeLineActivity.java
+++ app/src/main/java/com/indieweb/indigenous/TimeLineActivity.java
@@ -2,35 +2,56 @@ package com.indieweb.indigenous;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class TimeLineActivity extends AppCompatActivity {
+import com.kennyc.bottomsheet.BottomSheet;
+import com.kennyc.bottomsheet.BottomSheetListener;
+
+public class TimeLineActivity extends AppCompatActivity implements View.OnClickListener, BottomSheetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-
-        FloatingActionButton note = findViewById(R.id.noteButton);
-        note.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent CreateNote = new Intent(getBaseContext(), NoteActivity.class);
-                startActivity(CreateNote);
-            }
-        });
-
-        FloatingActionButton article = findViewById(R.id.articleButton);
-        article.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent CreateArticle = new Intent(getBaseContext(), ArticleActivity.class);
-                startActivity(CreateArticle);
-            }
-        });
-
+        findViewById(R.id.actionButton).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.actionButton:
+                new BottomSheet.Builder(this)
+                        .setSheet(R.menu.menu)
+                        .setListener(this)
+                        .show();
+                break;
+        }
+    }
+
+    @Override
+    public void onSheetShown(@NonNull BottomSheet bottomSheet, @Nullable Object object) {}
+
+    @Override
+    public void onSheetItemSelected(@NonNull BottomSheet bottomSheet, MenuItem menuItem, @Nullable Object o) {
+        switch (menuItem.getItemId()) {
+            case R.id.createArticle:
+                Intent CreateArticle = new Intent(getBaseContext(), ArticleActivity.class);
+                startActivity(CreateArticle);
+                break;
+            case R.id.createNote:
+                Intent CreateNote = new Intent(getBaseContext(), NoteActivity.class);
+                startActivity(CreateNote);
+                break;
+        }
+    }
+
+    @Override
+    public void onSheetDismissed(@NonNull BottomSheet bottomSheet, @Nullable Object o, int i) {}
 }
