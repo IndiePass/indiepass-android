989670e8ec56e4f2f3db2a2dd262d07076a5991d
swentel <swentel@realize.be>
2018-03-25 19:29:40 +0100

    Use viewHolders so items don't repeat themself

--- app/src/main/java/com/indieweb/indigenous/TimelineListAdapter.java
+++ app/src/main/java/com/indieweb/indigenous/TimelineListAdapter.java
@@ -2,6 +2,7 @@ package com.indieweb.indigenous;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,10 +22,12 @@ public class TimelineListAdapter extends BaseAdapter implements OnClickListener 
 
     private final Context context;
     private final List<TimelineItem> items;
+    private LayoutInflater mInflater;
 
     public TimelineListAdapter(Context context, List<TimelineItem> items) {
         this.context = context;
         this.items = items;
+        this.mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     public int getCount() {
@@ -39,44 +42,56 @@ public class TimelineListAdapter extends BaseAdapter implements OnClickListener 
         return position;
     }
 
-    public void onClick(View view) {
+    public void onClick(View view) {}
 
+    public static class ViewHolder {
+        public TextView author;
+        public TextView name;
+        public TextView content;
+        public ImageView image;
+        public LinearLayout row;
     }
 
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
 
         if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.timeline_list_item, null);
+            convertView = mInflater.inflate(R.layout.timeline_list_item, null);
+            holder = new ViewHolder();
+            holder.author = convertView.findViewById(R.id.timeline_author);
+            holder.name = convertView.findViewById(R.id.timeline_name);
+            holder.content = convertView.findViewById(R.id.timeline_content);
+            holder.image = convertView.findViewById(R.id.timeline_image);
+            holder.row = convertView.findViewById(R.id.timeline_item_row);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder)convertView.getTag();
         }
 
         final TimelineItem item = items.get(position);
         if (item != null) {
 
-            // Change color of row.
-            assert convertView != null;
             String color = ((position % 2) == 0) ? "#f8f7f1" :  "#ffffff";
-            final LinearLayout row = convertView.findViewById(R.id.timeline_item_row);
-            row.setBackgroundColor(Color.parseColor(color));
+            holder.row.setBackgroundColor(Color.parseColor(color));
 
             // Author.
-            final TextView author = convertView.findViewById(R.id.timeline_author);
-            author.setText(item.getAuthorName());
+            holder.author.setText(item.getAuthorName());
 
             // Name.
-            final TextView name = convertView.findViewById(R.id.timeline_name);
-            name.setText(item.getName());
+            holder.name.setText(item.getName());
 
             // Content.
-            final TextView content = convertView.findViewById(R.id.timeline_content);
-            content.setText(item.getContent());
+            holder.content.setText(item.getContent());
 
             // Image.
-            /*final ImageView image = convertView.findViewById(R.id.timeline_image);
             if (item.getPhoto().length() > 0) {
-                Glide.with(context).load(item.getPhoto()).into(image);
-            }*/
-
+                Glide.with(context).load(item.getPhoto()).into(holder.image);
+                holder.image.setVisibility(View.VISIBLE);
+            }
+            else {
+                holder.image.setVisibility(View.GONE);
+            }
         }
 
         return convertView;
