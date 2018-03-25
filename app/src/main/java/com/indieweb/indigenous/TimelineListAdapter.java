d26add4739397c225d9ae8e8a46cc0c5ea644ab0
swentel <swentel@realize.be>
2018-03-25 20:02:05 +0100

    Use an expander for very long posts (could become an action button later to read everything in a different activity, but that's fine for now)

--- app/src/main/java/com/indieweb/indigenous/TimelineListAdapter.java
+++ app/src/main/java/com/indieweb/indigenous/TimelineListAdapter.java
@@ -2,18 +2,21 @@ package com.indieweb.indigenous;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
+import at.blogc.android.views.ExpandableTextView;
 
 import java.util.List;
+
 
 /**
  * Timeline items list adapter.
@@ -47,13 +50,14 @@ public class TimelineListAdapter extends BaseAdapter implements OnClickListener 
     public static class ViewHolder {
         public TextView author;
         public TextView name;
-        public TextView content;
+        public Button expand;
+        public ExpandableTextView content;
         public ImageView image;
         public LinearLayout row;
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
+        final ViewHolder holder;
 
         if (convertView == null) {
             convertView = mInflater.inflate(R.layout.timeline_list_item, null);
@@ -61,6 +65,8 @@ public class TimelineListAdapter extends BaseAdapter implements OnClickListener 
             holder.author = convertView.findViewById(R.id.timeline_author);
             holder.name = convertView.findViewById(R.id.timeline_name);
             holder.content = convertView.findViewById(R.id.timeline_content);
+            holder.content.setInterpolator(new OvershootInterpolator());
+            holder.expand = convertView.findViewById(R.id.timeline_content_more);
             holder.image = convertView.findViewById(R.id.timeline_image);
             holder.row = convertView.findViewById(R.id.timeline_item_row);
             convertView.setTag(holder);
@@ -83,6 +89,27 @@ public class TimelineListAdapter extends BaseAdapter implements OnClickListener 
 
             // Content.
             holder.content.setText(item.getContent());
+            if (item.getContent().length() > 400) {
+                holder.expand.setVisibility(View.VISIBLE);
+                holder.expand.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(final View v) {
+                        if (holder.content.isExpanded()) {
+                            holder.content.collapse();
+                            holder.expand.setText(R.string.read_more);
+                        }
+                        else {
+                            holder.content.expand();
+                            holder.expand.setText(R.string.close);
+                        }
+                    }
+                });
+
+            }
+            else {
+                holder.expand.setVisibility(View.GONE);
+            }
 
             // Image.
             if (item.getPhoto().length() > 0) {
