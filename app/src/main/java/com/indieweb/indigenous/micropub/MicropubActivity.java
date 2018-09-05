85d1c425b71cfb8ad9a94ece4021e8d315d32176
swentel <swentel@realize.be>
2018-09-05 20:38:09 +0200

    Issue #70: styling of micropub screen

--- app/src/main/java/com/indieweb/indigenous/micropub/MicropubActivity.java
+++ app/src/main/java/com/indieweb/indigenous/micropub/MicropubActivity.java
@@ -1,7 +1,6 @@
 package com.indieweb.indigenous.micropub;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
