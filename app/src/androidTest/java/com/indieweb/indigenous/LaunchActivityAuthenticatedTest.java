f7a249d44291844f1ecc3b19ea8e590f6bff1bd2
swentel <swentel@realize.be>
2019-03-10 15:02:32 +0100

    Issue #50: create test utility class

--- app/src/androidTest/java/com/indieweb/indigenous/LaunchActivityAuthenticatedTest.java
+++ app/src/androidTest/java/com/indieweb/indigenous/LaunchActivityAuthenticatedTest.java
@@ -1,18 +1,12 @@
 package com.indieweb.indigenous;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
-
-import com.indieweb.indigenous.model.User;
-import com.indieweb.indigenous.util.Accounts;
 
 import org.junit.After;
 import org.junit.Before;
@@ -20,13 +14,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.content.Context.MODE_PRIVATE;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.indieweb.indigenous.indieauth.IndieAuthActivity.ACCOUNT_TYPE;
-import static com.indieweb.indigenous.indieauth.IndieAuthActivity.TOKEN_TYPE;
 
 @RunWith(AndroidJUnit4.class)
 public class LaunchActivityAuthenticatedTest extends ActivityInstrumentationTestCase2<LaunchActivity> {
@@ -43,27 +34,8 @@ public class LaunchActivityAuthenticatedTest extends ActivityInstrumentationTest
     @Before
     public void setUp() throws Exception {
         super.setUp();
-
         Context context = (InstrumentationRegistry.getTargetContext());
-
-        String domain = "http://example.com";
-        String accessToken = "awesome";
-
-        AccountManager am = AccountManager.get(context);
-        Account account = new Account(domain, ACCOUNT_TYPE);
-        am.addAccountExplicitly(account, null, null);
-        am.setAuthToken(account, TOKEN_TYPE, accessToken);
-        am.setUserData(account, "micropub_endpoint", domain + "/micropub");
-        am.setUserData(account, "authorization_endpoint", domain + "/auth");
-        am.setUserData(account, "token_endpoint", domain + "/token");
-        am.setUserData(account, "author_name", "Indigenous");
-
-        // Set first account.
-        SharedPreferences.Editor editor = context.getSharedPreferences("indigenous", MODE_PRIVATE).edit();
-        editor.putString("account", domain);
-        editor.apply();
-        editor.commit();
-
+        TestUtils.createAccount(context, false);
         Intent intent = new Intent();
         launchedActivity = rule.launchActivity(intent);
     }
@@ -72,14 +44,7 @@ public class LaunchActivityAuthenticatedTest extends ActivityInstrumentationTest
     public void tearDown() throws Exception {
         super.tearDown();
         Context context = (InstrumentationRegistry.getTargetContext());
-        User user = new Accounts(context).getCurrentUser();
-        AccountManager am = AccountManager.get(context);
-        am.removeAccount(user.getAccount(), launchedActivity, null, null);
-
-        SharedPreferences.Editor editor = context.getSharedPreferences("indigenous", MODE_PRIVATE).edit();
-        editor.putString("account", "");
-        editor.apply();
-        editor.commit();
+        TestUtils.removeAccount(context, launchedActivity);
     }
 
     @Test
