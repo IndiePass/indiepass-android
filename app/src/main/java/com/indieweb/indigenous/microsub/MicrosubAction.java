581af8dde0174052973ddf8bb78ad5238aa7c672
swentel <swentel@realize.be>
2020-02-13 21:08:18 +0100

    Issue #319

--- app/src/main/java/com/indieweb/indigenous/microsub/MicrosubAction.java
+++ app/src/main/java/com/indieweb/indigenous/microsub/MicrosubAction.java
@@ -32,18 +32,20 @@ public class MicrosubAction {
      * @param params
      *   The params to send.
      */
-    private void doMicrosubRequest(Map<String, String> params) {
+    private boolean doMicrosubRequest(Map<String, String> params) {
 
         if (!Utility.hasConnection(context)) {
             try {
                 Snackbar.make(layout, context.getString(R.string.no_connection), Snackbar.LENGTH_SHORT).show();
             }
             catch (Exception ignored) {}
-            return;
+            return false;
         }
 
         HTTPRequest r = new HTTPRequest(null, user, context);
         r.doPostRequest(user.getMicrosubEndpoint(), params);
+
+        return true;
     }
 
     /**
@@ -99,70 +101,62 @@ public class MicrosubAction {
      * Move post.
      */
     public void movePost(String channelId, String channelName, String postId) {
-
-        // TODO move where?
-        Snackbar.make(layout, String.format(context.getString(R.string.post_moved), channelName), Snackbar.LENGTH_SHORT).show();
-
         Map<String, String> params = new HashMap<>();
         params.put("action", "timeline");
         params.put("method", "move");
         params.put("channel", channelId);
         params.put("entry", postId);
-        doMicrosubRequest(params);
+        boolean success = doMicrosubRequest(params);
+        if (success) {
+            Snackbar.make(layout, String.format(context.getString(R.string.post_moved), channelName), Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * Create channel.
      */
     public void createChannel(final String channelName) {
-
-        // TODO - listener
-        Snackbar.make(layout, context.getString(R.string.channel_created), Snackbar.LENGTH_SHORT).show();
-
         Map<String, String> params = new HashMap<>();
         params.put("action", "channels");
         params.put("name", channelName);
-        doMicrosubRequest(params);
+        boolean success = doMicrosubRequest(params);
+        if (success) {
+            Snackbar.make(layout, context.getString(R.string.channel_created), Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * Update channel.
      */
     public void updateChannel(final String channelName, final String uid) {
-
-        // TODO - listener
-        Snackbar.make(layout, context.getString(R.string.channel_updated), Snackbar.LENGTH_SHORT).show();
-
         Map<String, String> params = new HashMap<>();
         params.put("action", "channels");
         params.put("channel", uid);
         params.put("name", channelName);
-        doMicrosubRequest(params);
+        boolean success = doMicrosubRequest(params);
+        if (success) {
+            Snackbar.make(layout, context.getString(R.string.channel_updated), Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * Delete channel.
      */
     public void deleteChannel(final String channelId) {
-
-        // TODO - listener
-        Snackbar.make(layout, context.getString(R.string.channel_deleted), Snackbar.LENGTH_SHORT).show();
-
         Map<String, String> params = new HashMap<>();
         params.put("action", "channels");
         params.put("method", "delete");
         params.put("channel", channelId);
-        doMicrosubRequest(params);
+        boolean success = doMicrosubRequest(params);
+        if (success) {
+            Snackbar.make(layout, context.getString(R.string.channel_deleted), Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * Order channels.
      */
     public void orderChannels(final List<Channel> Channels) {
-
-        // TODO - listener
-        Snackbar.make(layout, context.getString(R.string.channels_order_updated), Snackbar.LENGTH_SHORT).show();
-
         Map<String, String> params = new HashMap<>();
         params.put("action", "channels");
         params.put("method", "order");
@@ -171,37 +165,30 @@ public class MicrosubAction {
             params.put("channels[" + i + "]", c.getUid());
             i++;
         }
-        doMicrosubRequest(params);
+        boolean success = doMicrosubRequest(params);
+        if (success) {
+            Snackbar.make(layout, context.getString(R.string.channels_order_updated), Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * Delete channel.
      */
     public void deleteFeed(final String url, final String channelId) {
-
-        // TODO - listener
-        Snackbar.make(layout, context.getString(R.string.feed_deleted), Snackbar.LENGTH_SHORT).show();
-
         Map<String, String> params = new HashMap<>();
         params.put("action", "unfollow");
         params.put("url", url);
         params.put("channel", channelId);
-        doMicrosubRequest(params);
+        boolean success = doMicrosubRequest(params);
+        if (success) {
+            Snackbar.make(layout, context.getString(R.string.feed_deleted), Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * Subscribe.
      */
     public void subscribe(final String url, final String channelId, final boolean update) {
-
-        // TODO - listener
-        if (update) {
-            Snackbar.make(layout, context.getString(R.string.feed_updated), Snackbar.LENGTH_SHORT).show();
-        }
-        else {
-            Snackbar.make(layout, context.getString(R.string.feed_subscribed), Snackbar.LENGTH_SHORT).show();
-        }
-
         Map<String, String> params = new HashMap<>();
         if (update) {
             params.put("method", "update");
@@ -209,6 +196,14 @@ public class MicrosubAction {
         params.put("action", "follow");
         params.put("url", url);
         params.put("channel", channelId);
-        doMicrosubRequest(params);
+        boolean success = doMicrosubRequest(params);
+        if (success) {
+            if (update) {
+                Snackbar.make(layout, context.getString(R.string.feed_updated), Snackbar.LENGTH_SHORT).show();
+            }
+            else {
+                Snackbar.make(layout, context.getString(R.string.feed_subscribed), Snackbar.LENGTH_SHORT).show();
+            }
+        }
     }
 }
